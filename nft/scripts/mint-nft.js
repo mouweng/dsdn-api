4e452fce535146036a5199c451670ea82df4d6df
SimonSungm <simonsungm@gmail.com>
2021-12-30 20:40:07 +0800

    add nft

---
+++ nft/scripts/mint-nft.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+const contractAddress = "0xaC76Ac9995eb52AaDA526F53333942E49eC9A206";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+	console.log(nonce);
+  //the transaction
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 1999999987,
+    'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+  };
+
+
+  const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  const transactionReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+  console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
+}
+
+/*
+async function viewNFT(tokenID) {
+	const options = { address: PUBLIC_KEY, chain: "80001" };
+	const nftOwners = await web3.eth.getNFTOwners(options);
+
+	console.log(`NFT: ${JSON.stringify(nftOwner)}`);
+}
+
+*/
+
+mintNFT("https://gateway.pinata.cloud/ipfs/QmYueiuRNmL4MiA2GwtVMm6ZagknXnSpQnB3z2gWbz36hP");
+
